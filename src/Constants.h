064d6b6ba182575f0075c06995e0e15041da1201
Sunny To <sunny14539@yahoo.com>
2018-04-27 20:46:24 -0700

    Changed title of game

--- src/Constants.h
+++ src/Constants.h
@@ -19,16 +19,16 @@
 
 #include <string>
 
-const std::string APP_NAME =      "Witch Blast";
-const std::string APP_VERSION =   "0.7.5";
+const std::string APP_NAME =      "School Nightmare";
+const std::string APP_VERSION =   "0.0.1";
 
 const std::string CONFIG_FILE =     "config.dat";
 const std::string SAVE_FILE =       "game.sav";
 const std::string SAVE_DATA_FILE =  "data/data.sav";
 const std::string HISCORES_FILE =   "data/scores.dat";
 
-const std::string SAVE_VERSION =    "SAVE_0.7";
-const std::string SCORE_VERSION =   "V075";
+const std::string SAVE_VERSION =    "SAVE_0.1";
+const std::string SCORE_VERSION =   "V001";
 
 const int NB_LANGUAGES = 5;
 const std::string languageString[NB_LANGUAGES] = { "english", "french", "german", "spanish", "russian" };
